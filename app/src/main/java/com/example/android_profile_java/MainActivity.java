6e22319a1dac0b5d48e7c8fa4f88c268602737de
Techbite <bonfacemwema7@gmail.com>
2024-02-05 12:25:50 +0300

    implemented Mainactivity and its layout

--- app/src/main/java/com/example/android_profile_java/MainActivity.java
+++ app/src/main/java/com/example/android_profile_java/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android_profile_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set title
+        getSupportActionBar().setTitle("Firebase App");
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        Button buttonRegister = findViewById(R.id.button_register);
+
+        //open login activity
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //open Register activity
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
